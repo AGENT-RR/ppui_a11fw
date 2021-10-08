f1ef52a41f541e7a9c21b1fa930a593cf8f83dd4
Rahul Singh <demogorgon@pop-os.localdomain>
2021-10-08 20:26:32 +0530

    Raphael : PPUI Official Bumped to 3.10 Azure

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+# Inherit some common PixelPlusUI stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := ancient_raphael
+PRODUCT_NAME := aosp_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
@@ -26,16 +26,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
-
-# Ancient stuff
+CUSTOM_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
-EXTRA_FOD_ANIMATIONS := true
-
-# Ancient
-ANCIENT_OFFICIAL=true
-#TARGET_GAPPS_ARCH := arm64
-#IS_PHONE := true
-#ANCIENT_GAPPS := true
-#ANCIENT_WEEABO := true
